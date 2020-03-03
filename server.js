ab3b210d9275010ecd48ca7454fbc168c6c65d34
Lucas Gehlen <contato.lucasgehlen@Gmail.com>
2020-03-03 14:04:31 -0300

    Adicionando CORS

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
 //Nodemon => para execução automática no nodeserver
 const express = require('express'); //Dependência de rotas => Express
-
+const cors = require('cors');
 //Iniciando o app
 const app = express();
 const Product = require('./src/models/Product');
 app.use(express.json()); //Permitindo envio de dados json
-
+//Utilizando o cors para acesso através de outros dominios
+app.use(cors());
 //Sempre que tiver /api vai fazer requisição do arquivo
 app.use('/api', require('./src/routes'));
 
